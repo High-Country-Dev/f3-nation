b445b6c807aeeb8200756e1d850eb253433ea2af
elsiedp <elsie@mountaindev.com>
2026-01-28 12:32:02 +0800

    Add hideClearButton prop to VirtualizedCombobox and AdminEventTypesModal

--- apps/map/src/app/_components/modal/admin-event-types-modal.tsx
+++ apps/map/src/app/_components/modal/admin-event-types-modal.tsx
@@ -223,6 +223,7 @@ export default function AdminEventTypesModal({
                         }
                       }}
                       isMulti={false}
+                      hideClearButton
                     />
                     <FormMessage />
                   </FormItem>
